03c46b61ed973cec2d6fad6929127df7cc643a99
huntingmarco <laquihonjannel@yahoo.com>
2021-04-04 22:14:12 -0400

    update portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Portfolio from '../components/Portfolio';
-import PortfolioVideo from '../components/PortfolioVideo';
 import ScrollToTop from '../components/ScrollToTop';
 import Footer from '../components/Footer';
 import Navbar from '../components/Nabvar';
